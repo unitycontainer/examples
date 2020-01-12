cc90c87dcd317ab1ab710ddd9673f099a347e678
Eugene Sadocoi <evgeni@eniks.com>
2020-01-11 16:43:27 -0800

    Added disposable project

---
+++ src/Container/ChildContainer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using Unity;
+
+namespace ChildContainer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var container = new UnityContainer();
+
+            var wr = CreateChild(container);
+
+            GC.Collect(GC.MaxGeneration, GCCollectionMode.Forced, true, true);
+
+            Debug.Assert(false == wr.IsAlive);
+        }
+
+        static WeakReference CreateChild(IUnityContainer container)
+        {
+            var child = container.CreateChildContainer();
+            var wr = new WeakReference(child);
+            child.Dispose();
+           
+            
+            return wr;
+        }
+    }
+}
